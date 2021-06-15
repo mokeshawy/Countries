d6deb8696c90910c8c39d49a4b981ba936c22148
mohamed <mohamed_gsm@msn.com>
2021-06-15 13:44:53 +0200

    finish implement navigation component

--- app/src/main/java/com/example/countries/MainActivity.kt
+++ app/src/main/java/com/example/countries/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.countries.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
     lateinit var navHostFragment: NavHostFragment
     lateinit var navController  : NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
@@ -20,5 +23,17 @@ class MainActivity : AppCompatActivity() {
         navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController   = navHostFragment.navController
 
+        // Operation work for action bar on fragment page.
+        appBarConfiguration =AppBarConfiguration(setOf())
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        // Work on hide and show action bar on fragment page.
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            when(destination.id){
+
+                else -> supportActionBar!!.show()
+            }
+        }
     }
 }
